aac130157b59ea5fdc4f953d332b6c5c610612c2
Your Name <test>
2024-04-03 02:59:25 +0200

    new impl

---
+++ src/main/java/config/FootballEndPoints.java
@@ -0,0 +1,8 @@
+package config;
+
+public interface FootballEndPoints {
+
+    String AREAS_ENDPOINT = "areas";
+    String AREA_ENDPOINT = "areas/{areaId}";
+    String POST_ENDPOINT = "areas";
+}
---
+++ src/test/java/apiTests/BaseApiTest.java
@@ -0,0 +1,15 @@
+package apiTests;
+
+import api.ApiTestConfig;
+import api.requests.ApiRequests;
+import org.testng.annotations.BeforeClass;
+
+public class BaseApiTest {
+    public static ApiRequests requests;
+
+    @BeforeClass
+    public void setUpTests(){
+        ApiTestConfig.setUp();
+        requests=new ApiRequests();
+    }
+}
